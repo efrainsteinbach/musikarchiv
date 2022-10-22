510c927e32d44d67a4860c49261132f85f7a6757
efrainsteinbach <git@github.com>
2022-10-22 13:09:26 +0200

    Added continuing on track ended

--- frontend/src/app/player/player.component.ts
+++ frontend/src/app/player/player.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Album, Track } from '../app.interfaces';
+import { Album } from '../app.interfaces';
 
 @Component({
   selector: 'app-player',
@@ -10,6 +10,7 @@ import { Album, Track } from '../app.interfaces';
 export class PlayerComponent implements AfterViewInit {
 
   album: Album;
+  indexOfSongPlaying: number = -1;
 
   audioElement!: HTMLAudioElement;
   audioSourceElement!: HTMLAudioElement;
@@ -24,12 +25,25 @@ export class PlayerComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.audioElement = this.audioRef.nativeElement;
     this.audioSourceElement = this.audioRef.nativeElement;
+
+    this.audioElement.addEventListener('ended', () => {
+      this.onSongEnded();
+    })
   }
 
   playSong(index: number) {
+    console.log(`starting playback of song #${index+1}`);
     const track = this.album.tracks[index];
     this.audioElement.src = track.url;
     this.audioElement.load();
     this.audioElement.play();
+    this.indexOfSongPlaying = index;
+  }
+
+  onSongEnded() {
+    console.log(`song #${this.indexOfSongPlaying} ended`);
+    if (this.indexOfSongPlaying < this.album.tracks.length - 1 && this.indexOfSongPlaying >= 0) {
+      this.playSong(this.indexOfSongPlaying + 1);
+    }
   }
 }
