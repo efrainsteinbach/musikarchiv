4961999cd867ce1c2e954b2fd233565992de7493
efrainsteinbach <git@github.com>
2022-11-03 18:22:07 +0100

    More minor mobile UX improvements

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Album } from './app.interfaces';
 import { HttpClient } from '@angular/common/http';
 import { MediaSessionConnectorService } from './mediasession-connector.service';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -16,19 +17,24 @@ export class AppComponent {
   albumDetail: Album | undefined;
 
   ngAfterViewInit(): void {
-    this.http.get<Album[]>("http://localhost:3000/index.json")
+    this.http.get<Album[]>(environment.musicIndexLocation)
       .subscribe(data => {
         this.albums = data;
+        console.log(data.map(x => x.year));
+        this.albums.sort((a, b) => b.year - a.year);
+        console.log(data.map(x => x.year));
         this.dataReady = true;
       });
   }
 
   showAlbum(album: Album) {
     this.albumDetail = album;
+    window.scrollTo(0, 0);
   }
 
   closeAlbum() {
     this.albumDetail = undefined;
+    window.scrollTo(0, 0);
   }
 }
 
